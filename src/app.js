1cea5edb8f80065ffbbb06a3c7402270f17e635d
Mar Aguayo Florit <154342626+magufl@users.noreply.github.com>
2024-03-24 20:01:38 +0000

    done

--- src/app.js
+++ src/app.js
@@ -2,41 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
-  let who = ["The dog", "My grandma", "The mailman", "My bird", "Remi"];
-
-  let action = ["ate", "peed", "crushed", "broke", "dance on top of"];
-
-  let what = [
-    "my homework",
-    "my phone",
-    "the car",
-    "Empire State",
-    "the dead sea"
-  ];
-
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  let frase = [who + action + what + when];
-  /*  document.querySelector("#excuse").innerHTML = randomnumber; */
-
-  function NUMRANDOM(NUM) {
-    return Math.floor(Math.random() * NUM);
-  }
-
-  let text = " ";
-  frase.map(item => {
-    text += "item.who";
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = resultado();
   });
 
-  console.log(frase);
+  let resultado = () => {
+    let who = ["The dog", "My grandma", "The mailman", "My bird", "Remi"];
+    let action = ["ate", "peed", "crushed", "broke", "dance on top of"];
+    let what = [
+      "my homework",
+      "my phone",
+      "the car",
+      "Empire State",
+      "the dead sea"
+    ];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let aleatoriowho = Math.floor(Math.random() * who.length);
+    let aleatorioaction = Math.floor(Math.random() * action.length);
+    let aleatoriowhat = Math.floor(Math.random() * what.length);
+    let aleatoriowhen = Math.floor(Math.random() * when.length);
+
+    return (
+      who[aleatoriowho] +
+      " " +
+      action[aleatorioaction] +
+      " " +
+      what[aleatoriowhat] +
+      " " +
+      when[alealeatoriowhenatoriocuando]
+    );
+  };
 };
