1de69a8eacca5f6dc8027cbee075408e2ec550ce
Mar Aguayo Florit <154342626+magufl@users.noreply.github.com>
2024-03-24 20:01:24 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird", "Remi"];
+
+  let action = ["ate", "peed", "crushed", "broke", "dance on top of"];
+
+  let what = [
+    "my homework",
+    "my phone",
+    "the car",
+    "Empire State",
+    "the dead sea"
+  ];
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let frase = [who + action + what + when];
+  /*  document.querySelector("#excuse").innerHTML = randomnumber; */
+
+  function NUMRANDOM(NUM) {
+    return Math.floor(Math.random() * NUM);
+  }
+
+  let text = " ";
+  frase.map(item => {
+    text += "item.who";
+  });
+
+  console.log(frase);
 };
